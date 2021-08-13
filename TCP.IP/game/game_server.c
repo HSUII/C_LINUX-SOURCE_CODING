2b3e2220d0a13c41d0b909732fb6ff47dc3282f1
수이_SUI <87008955+HSUII@users.noreply.github.com>
2021-08-13 16:28:29 +0900

    Update game_server.c

--- TCP.IP/game/game_server.c
+++ TCP.IP/game/game_server.c
@@ -63,9 +63,39 @@
                                 if(randnum==0){
                                         hint = "힌트: 이 동물은 집사를 거느리고 있다.\n";
                                         strcpy(Buffer,hint);
-                        n = strlen(Buffer);
+                                        n = strlen(Buffer);
                                         write(c_socket,Buffer,n);
                                 }
+                                if(randnum==1){
+                                        hint = "힌트: 이 동물은 사람과 유대감이 가장 깊은 동물이다..\n";
+                                        strcpy(Buffer,hint);
+                                        n = strlen(Buffer);
+                                        write(c_socket,Buffer,n);
+                                }
+                                 if(randnum==2){
+                                        hint = "힌트: 이 동물은 거북이에게 경주에서 진 동물이다.\n";
+                                        strcpy(Buffer,hint);
+                                        n = strlen(Buffer);
+                                        write(c_socket,Buffer,n);
+                                }
+                                if(randnum==3){
+                                        hint = "힌트: 이 동물은 사람에게 비행기의 영감을 준 동물이다.\n";
+                                        strcpy(Buffer,hint);
+                                        n = strlen(Buffer);
+                                        write(c_socket,Buffer,n);
+                                } 
+                                if(randnum==4){
+                                        hint = "힌트: 이 동물은 매끈하고 귀엽게 생겼으며 바다에 사는 동물이다.\n";
+                                        strcpy(Buffer,hint);
+                                        n = strlen(Buffer);
+                                        write(c_socket,Buffer,n);
+                                }
+                                if(randnum==5){
+                                        hint = "힌트: 이 동물은 자신의 무리를 끔찍히 생각하며 평생 한 반려만을 둔다.\n";
+                                        strcpy(Buffer,hint);
+                                        n = strlen(Buffer);
+                                        write(c_socket,Buffer,n);
+                                }         
                 rBuffer[length] = '\0';
                 write(c_socket, rBuffer,n);
                 if(!strcmp(rBuffer,answer)){
